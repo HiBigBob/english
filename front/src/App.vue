3adb38cbee37d34c77fb57cc80c977ca9c18a062
Adrien Bouttier <hibigbob@gmail.com>
2019-02-22 15:52:56 +0100

    First brick

---
+++ front/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <h1>Practice</h1>
+    <HelloWorld msg="Practice"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    display: flex;           /* establish flex container */
+    flex-direction: column;  /* make main axis vertical */
+    justify-content: center; /* center items vertically, in this case */
+    align-items: center;     /* center items horizontally, in this case */
+    height: 300px;
+}
+
+h1 {
+    margin-top: 30px;
+}
+</style>
---
+++ front/src/components/HelloWorld.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="hello">
+        <div class="form-signin">
+            <input type="text" id="inputEmail" class="form-control" placeholder="Response" @keyup="keymonitor" autofocus>
+            <button class="btn btn-sm btn-primary" >Help</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HelloWorld',
+    props: {
+        msg: String
+    },
+    methods: {
+        keymonitor(event) {
+            console.log('keyup from id: '+event.target.id)
+
+            // what was pressed?
+            let keyMessage = 'keyup: ';
+            if (event.shiftKey) {
+                keyMessage += 'Shift+';
+            }
+            keyMessage += event.key || String.fromCharCode(event.keyCode);
+
+            console.log(keyMessage)
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.inputEngine {
+    color: #2c3e50;
+    font-weight: bold;
+    border: 1px solid grey;
+    border-radius: 5px;
+    padding: 5px;
+}
+h3 {
+    margin: 40px 0 0;
+}
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+a {
+    color: #42b983;
+}
+
+/*
+ * Globals
+ */
+
+.form-signin {
+    width: 100%;
+    max-width: 330px;
+    padding: 15px;
+    margin: auto;
+}
+
+.form-signin .form-control {
+    position: relative;
+    box-sizing: border-box;
+    height: auto;
+    padding: 10px;
+    font-size: 16px;
+}
+
+textarea:focus, input:focus{
+    outline: none;
+}
+
+.form-control:focus {
+  border-color: inherit;
+  -webkit-box-shadow: none;
+  box-shadow: none;
+}
+
+.form-signin input[type="text"] {
+    margin-bottom: 20px;
+}
+
+</style>
