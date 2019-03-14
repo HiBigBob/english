068b8bb5b20f26c68b1fe86919a4c1e214c0c0b3
Adrien Bouttier <hibigbob@gmail.com>
2019-03-14 08:16:43 +0100

    Create test.js

---
+++ api/src/routes/test.js
@@ -0,0 +1,12 @@
+
+import Abstract from './abstract'
+
+export default class Test extends Abstract {
+  constructor() {
+    super()
+
+    this.router.get('/', async (ctx, next) => {
+      ctx.ok(await ctx.db.collection('tests').find({}).toArray())
+    })
+  }
+}
