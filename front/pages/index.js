67debb712d8084f57d57224b5a44f9163e9966f8
Adrien Bouttier <hibigbob@gmail.com>
2019-03-19 22:42:50 +0100

    Add auth on main page

--- front/pages/index.js
+++ front/pages/index.js
@@ -6,18 +6,22 @@ import KeyBoard from '../components/KeyBoard'
 
 import { loadData, startClock, tickClock } from '../store/actions'
 import Layout from '../components/Layout'
+import { auth } from '../utils/auth'
 
 class Index extends React.Component {
-  static async getInitialProps (props) {
-    const { store, isServer } = props.ctx
+  static async getInitialProps ({ctx}) {
+    const token = auth(ctx)
+
+    const { store, isServer } = ctx
     store.dispatch(tickClock(isServer))
 
     if (!store.getState().placeholderData) {
       store.dispatch(loadData())
     }
 
-    return { isServer }
+    return { isServer, token }
   }
+
 
   componentDidMount () {
     this.props.dispatch(startClock())
