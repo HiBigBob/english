2131950599a13516feb7127dce11026c07cfe283
Adrien Bouttier <hibigbob@gmail.com>
2019-03-21 08:31:55 +0100

    Change indent

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -10,42 +10,42 @@ import Router from 'next/router'
 import createStore from '../store/store'
 
 Router.events.on('routeChangeStart', url => {
-    console.log(`Loading: ${url}`)
-    NProgress.start()
+  console.log(`Loading: ${url}`)
+  NProgress.start()
 })
 Router.events.on('routeChangeComplete', () => NProgress.done())
 Router.events.on('routeChangeError', () => NProgress.done())
 
 class MyApp extends App {
-    static async getInitialProps ({ Component, ctx }) {
-        let pageProps = {}
+  static async getInitialProps ({ Component, ctx }) {
+    let pageProps = {}
 
-        if (Component.getInitialProps) {
-            pageProps = await Component.getInitialProps({ ctx })
-        }
-
-        return { pageProps }
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps({ ctx })
     }
 
-    componentDidMount() {
-		const loader = document.getElementById('initLoader');
-		if(loader) {
-            setTimeout(() => {
-                loader.classList.add('hidden');
-            }, 600);
-		}
-    }
+    return { pageProps }
+  }
 
-    render () {
-        const { Component, pageProps, store } = this.props
-        return (
-            <Container>
-                <Provider store={store}>
-                    <Component {...pageProps} />
-                </Provider>
-            </Container>
-        )
+  componentDidMount() {
+    const loader = document.getElementById('initLoader');
+    if(loader) {
+      setTimeout(() => {
+        loader.classList.add('hidden');
+      }, 600);
     }
+  }
+
+  render () {
+    const { Component, pageProps, store } = this.props
+    return (
+      <Container>
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
+      </Container>
+    )
+  }
 }
 
 export default withRedux(createStore)(withReduxSaga(MyApp));
